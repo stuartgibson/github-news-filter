def9703861eac995e686b6c388bcf025fdf42fe9
Stuart <stuart@abovetheinternet.org>
2015-06-17 12:52:00 +0100

    Persist status with localStorage, moved into browser folder

--- safari/Github news filter.safariextension/github-news-filter.js
+++ safari/Github news filter.safariextension/github-news-filter.js
@@ -9,14 +9,21 @@ $(document).ready(function(){
                         issues_comment: "Comments"
                       }
 
+  default_status = {}
+
   var injectedContent = '<div id="abovetheinternet-github-news-filter">'
 
   $.each(activityTypes, function (key, value) {
+    default_status[key] = (localStorage['current_status_' + key] || "true");
+
+    if ( default_status[key] == "false" ){
+      $("#dashboard").addClass("hide-" + key);}
+
     injectedContent +=  '<label for="abovetheinternet-github-news-filter-' + key + '">' +
                           '<input type="checkbox"' +
                                   'class="abovetheinternet-github-news-filter-toggle"' +
                                   'name="filter-' + key + '"' +
-                                  'checked="checked"' +
+                                  (default_status[key] == "true" ? 'checked="checked"' : '') +
                                   'id="abovetheinternet-github-news-filter-' + key + '"' +
                                   'data-activity-type="' + key + '"> ' +
                             value +
@@ -34,19 +41,26 @@ $(document).ready(function(){
   $("#dashboard .news").prepend(injectedContent);
 
   $("body").on("change", "#abovetheinternet-github-news-filter .abovetheinternet-github-news-filter-toggle", function(){
-    $("#dashboard").toggleClass("hide-" + $(this).data("activity-type"))
+    $("#dashboard").toggleClass("hide-" + $(this).data("activity-type"));
+    localStorage['current_status_' + $(this).data("activity-type")] = $(this).prop("checked");
   });
 
   $("body").on("click", "#abovetheinternet-github-news-filter-all", function(e){
     e.preventDefault();
     $(".abovetheinternet-github-news-filter-toggle").prop("checked", true);
-    $.each(activityTypes, function (key, value) { $("#dashboard").removeClass("hide-" + key); });
+    $.each(activityTypes, function (key, value) {
+      $("#dashboard").removeClass("hide-" + key);
+      localStorage['current_status_' + key] = true;
+    });
   });
 
   $("body").on("click", "#abovetheinternet-github-news-filter-none", function(e){
     e.preventDefault();
     $(".abovetheinternet-github-news-filter-toggle").prop("checked", false);
-    $.each(activityTypes, function (key, value) { $("#dashboard").addClass("hide-" + key); });
+    $.each(activityTypes, function (key, value) {
+      $("#dashboard").addClass("hide-" + key);
+      localStorage['current_status_' + key] = false;
+    });
   });
 
 });
